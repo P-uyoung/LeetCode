1643ea6fa1655caf8aa3917cddf1de19adb24be2
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-04-14 23:09:50 +0900

    Time: 33 ms (57.49%), Space: 13.9 MB (23.95%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,16 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo = {}
+        dp = {}
         for y in range(m):
-            memo[(0,y)] = 1
+            dp[(0,y)] = 1
         for x in range(n):
-            memo[(x,0)] = 1
-        
-        def pathCount(coordinate):
-            if coordinate not in memo:
-                x, y = coordinate
-                memo[(x,y)] = pathCount((x,y-1)) + pathCount((x-1,y))
-            return memo[coordinate]
-        
-        return pathCount((n-1,m-1))
+            dp[(x,0)] = 1
+            
+        for x in range(1,n):
+            for y in range(1,m):
+                dp[(x,y)] = dp[(x-1,y)] + dp[(x,y-1)] 
+                
+        return dp[(n-1,m-1)]
             
