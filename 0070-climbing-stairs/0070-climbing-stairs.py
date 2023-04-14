d0b4ff9320ae28aeea73d3e7ecc31a8822dc8296
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-04-14 16:48:22 +0900

    Time: 39 ms (9.20%), Space: 14 MB (6.36%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def dfs(two_count, one_count):
-            if two_count == 0 :
-                return 1
-            result = (math.factorial(two_count + one_count) / (math.factorial(two_count) * math.factorial(one_count)))
-            return int(result) + dfs(two_count-1, one_count+2)
-        
-        return dfs(n//2, n%2)
-        
+        memo = {1 : 1, 2 : 2}
+        def cs(n):
+            if n not in memo:
+                memo[n]= cs(n-1) + cs(n-2)
+            return memo[n]
+        return cs(n)
+          
