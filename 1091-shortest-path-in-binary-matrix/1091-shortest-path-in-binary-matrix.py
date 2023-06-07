f4c19a13bbda4d652639687026041dcf9ca252bc
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-06-07 22:50:21 +0900

    Time: 605 ms (61.92%), Space: 16.6 MB (92.88%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,32 +1,27 @@
-class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
-            return -1 
-        if n == 1:
+class Solution(object):
+    def shortestPathBinaryMatrix(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        N = len(grid)
+        if grid[0][0] or grid[N-1][N-1]:
+            return -1
+        if N == 1:
             return 1
-        
-        # shortest = [[10001]*n for _ in range(n)]  # BFS 먼저 도착한 게 최단거리임. shortest 필요 없음.
-        dx = [0,0,1,-1,1,1,-1,-1]
-        dy = [1,-1,0,0,1,-1,1,-1]
         q = deque()
-        grid[0][0] = 1                              # 방문 처리
         q.append((0,0,1))
-        
+        dx = (0, 0, 1, -1, 1, 1, -1, -1)
+        dy = (1, -1, 0, 0, -1, 1, 1, -1)
+
         while q:
             x, y, dist = q.popleft()
-            for i in range(8):                      # 방문 예약
-                next_x = x + dx[i]
-                next_y = y + dy[i]
-                if next_x < 0 or next_x >= n or next_y < 0 or next_y >= n:
-                    continue
-                if grid[next_y][next_x] == 1:
-                    continue
-                if next_x == n-1 and next_y == n-1:
+            for i in range(8):
+                nx, ny = x+dx[i], y+dy[i]
+                if nx == N-1 and ny == N-1:
                     return dist + 1
-                else:
-                    grid[next_y][next_x] = 1
-                    q.append((next_x, next_y, dist+1))
-        
+                elif 0 <= nx < N and 0 <= ny < N and not grid[ny][nx]:
+                    q.append((nx,ny,dist+1))
+                    grid[ny][nx] = 1
+
         return -1
-        
