67839ff5bdb8bdc822cd40efb708ade844b6d2dd
P-uyoung <48639021+P-uyoung@users.noreply.github.com>
2023-06-08 19:25:35 +0900

    Time: 50 ms (46.89%), Space: 16.5 MB (70.06%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,25 +1,16 @@
-from itertools import combinations
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        open_loca = list(combinations([i-1 for i in range(2*n)], n))
-        output = []
-        
-        for idxs in open_loca:
-            arr = [1 if i in idxs else -1 for i in range(2*n)]
-            sum, i = 0, 0
-            while i < 2*n:
-                if sum < 0:
-                    break
-                sum += arr[i]
-                i += 1
-            
-            if i == 2*n:
-                output.append("".join(["(" if x == 1 else ")" for x in arr]))
-                
-        return output
-                
-                
-        
-                
-                
-                        
+        def dfs(left, right, s):
+            if len(s) == n * 2:
+                res.append(s)
+                return 
+
+            if left < n:
+                dfs(left + 1, right, s + '(')
+
+            if right < left:
+                dfs(left, right + 1, s + ')')
+
+        res = []
+        dfs(0, 0, '')
+        return res
