d7587af6a90b52bf9cb344f517e2ee519f47ce00
WooyoungPark <48639021+P-uyoung@users.noreply.github.com>
2024-01-29 20:48:22 +0900

    Time: 56 ms (86.67%), Space: 17.7 MB (72.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic = {}
-        for i, num in enumerate(nums):
-            dic[num] = i
-        
-        for i, num in enumerate(nums):
-            key = target - num
-            if key in dic:  # BigO(1)
-                if i == dic[key]: continue
-                else:
-                    return [i, dic[key]]
-                
-        
+        for i, n in enumerate(nums):
+            if target-n in dic:
+                return dic[target-n], i
+            else:
+                dic[n] = i
